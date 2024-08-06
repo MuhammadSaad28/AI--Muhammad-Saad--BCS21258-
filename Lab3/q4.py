3fc91183f99128828746886cf182b1e0a9ccf82e
MuhammadSaad28 <muhammadsaad28102003@gmail.com>
2024-08-06 09:40:52 +0500

    Lab 6

--- Lab3/q4.py
+++ Lab3/q4.py
@@ -14,6 +14,7 @@ def pattern():
         for j in range(i):
             print("*", end="")
         print()
+        
     for i in range(4, 0, -1):
         for j in range(i):
             print("*", end="")
@@ -24,3 +25,63 @@ def main():
     
 if __name__ == "__main__":
     main()
+    
+    
+# give more patterns to print
+# *
+# **
+# ***
+# ****
+# *****
+# ****
+# ***
+# **
+# *
+
+
+#      *
+#     * *
+#    * * *
+#   * * * *
+# solve this pattern
+
+#      *
+#     * *
+#    * * *
+#   * * * *
+#  * * * * *
+#   * * * *
+#    * * *
+#     * *
+#      *
+# give the code for 
+#      *
+#     * *
+#    * * *
+#   * * * *
+#  * * * * *
+#   * * * *
+#    * * *
+#     * *
+#      *
+
+
+def pattern1():
+    for i in range(1, 6):
+        for j in range(5-i):
+            print(" ", end="")
+        for j in range(i):
+            print("*", end=" ")
+        print()
+    for i in range(4, 0, -1):
+        for j in range(5-i):
+            print(" ", end="")
+        for j in range(i):
+            print("*", end=" ")
+        print()
+        
+pattern1()
+
+
+
+        
