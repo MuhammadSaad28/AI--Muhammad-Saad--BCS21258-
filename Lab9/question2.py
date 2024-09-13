f5c5d230af561a4bab10fe09287335ea9cc598a4
MuhammadSaad28 <muhammadsaad28102003@gmail.com>
2024-09-14 00:22:04 +0500

    .

--- Lab9/question2.py
+++ Lab9/question2.py
@@ -1,13 +1,14 @@
-# Implement alpha beta pruning in python. .(pseudocode is given in 
+# Implement alpha beta pruning in python. .(pseudocode is given in
 # chapter slides as well as in this manual below)
 
 # Code:
+
 
 def minimax(node, depth, isMaximizingPlayer, alpha, beta):
     if depth == 0 or len(node[1]) == 0:
         return node[0]
     if isMaximizingPlayer:
-        bestVal = float('-inf')
+        bestVal = float("-inf")
         for child in node[1]:
             value = minimax(child, depth - 1, False, alpha, beta)
             bestVal = max(bestVal, value)
@@ -16,7 +17,7 @@ def minimax(node, depth, isMaximizingPlayer, alpha, beta):
                 break
         return bestVal
     else:
-        bestVal = float('inf')
+        bestVal = float("inf")
         for child in node[1]:
             value = minimax(child, depth - 1, True, alpha, beta)
             bestVal = min(bestVal, value)
@@ -24,8 +25,17 @@ def minimax(node, depth, isMaximizingPlayer, alpha, beta):
             if beta <= alpha:
                 break
         return bestVal
-    
-tree = [3, [[5, [[9, [[8, [], []], [6, [], []]]], [6, [], []]], [2, [[3, [[1, [], []]], [7, [], []]]]]]]]
 
-print(minimax(tree, 3, True, float('-inf'), float('inf'))) # 5
 
+tree = [
+    3,
+    [
+        [
+            5,
+            [[9, [[8, [], []], [6, [], []]]], [6, [], []]],
+            [2, [[3, [[1, [], []]], [7, [], []]]]],
+        ]
+    ],
+]
+
+print(minimax(tree, 3, True, float("-inf"), float("inf")))  # 5
