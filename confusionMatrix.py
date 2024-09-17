b406e908685a28b4ef30eae6f1b13d14f213f861
MuhammadSaad28 <muhammadsaad28102003@gmail.com>
2024-09-17 21:46:38 +0500

    .

--- confusionMatrix.py
+++ confusionMatrix.py
@@ -1,10 +1,10 @@
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
 
 # True labels (actual values)
-y_true = [1, 0, 1, 1, 0, 1, 0, 0, 1, 0]
+y_true = [1]*50 + [1]*10 + [0]*35 + [0]*5  # 50 actual positives, 10 false negatives, 35 actual negatives, 5 false positives
 
 # Predicted labels (by the model)
-y_pred = [1, 0, 1, 0, 0, 1, 0, 1, 1, 0]
+y_pred = [1]*50 + [0]*10 + [0]*35 + [1]*5  # 50 true positives, 10 false negatives, 35 true negatives, 5 false positives
 
 # Confusion matrix
 cm = confusion_matrix(y_true, y_pred)
